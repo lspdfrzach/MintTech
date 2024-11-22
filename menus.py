3f24b208333c295b7748f00bb1a7a0399b3aa757
Shashank Pandey <jpshashank200@gmail.com>
2024-11-22 14:13:39 +0530

    Updated menu.py

--- menus.py
+++ menus.py
@@ -8076,9 +8076,11 @@ class ERLCIntegrationConfiguration(AssociationConfigurationView):
             ).add_field(
                 name="Auto Kick/Ban Logging",
                 value="This is where you can configure the auto kick/ban feature for ER:LC.",
+                inline=False
             ).add_field(
                 name="Discord Check Message",
-                value="This message is sent to in-game users when a Discord Check is performed with bot if user is not found in server."
+                value="This message is sent to in-game users when a Discord Check is performed with bot if user is not found in server.",
+                inline=False
             ),
             view=view,
             ephemeral=True
